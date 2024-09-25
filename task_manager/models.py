de5ffad2a9bcea1dc23c3914bd4814125c45af4a
Maksym Protsak <maksym.protsak@gmail.com>
2024-09-25 14:22:00 +0300

    Added models in task_manager app

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+TASK_PRIORITIES = [
+    ("bl", "Blocker"),
+    ("cr", "Critical"),
+    ("mj", "Major"),
+    ("mn", "Minor"),
+    ("tr", "Trivial")
+]
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    deadline = models.DateField
+    is_completed = models.BooleanField()
+    priority = models.CharField(max_length=1, choices=TASK_PRIORITIES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
