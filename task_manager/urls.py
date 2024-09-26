358b67fa11b62a457775e13df3b3acda2646564d
Maksym Protsak <maksym.protsak@gmail.com>
2024-09-26 13:43:12 +0300

    Added djungo debug toolbar

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import (
     index,
 )
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("", index, name="index"),
-]
+] + debug_toolbar_urls()
 
 app_name = "task_manager"
