37d691e05330d0dd4fa4632047dca0eddebd2afe
Maksym Protsak <maksym.protsak@gmail.com>
2024-09-28 13:00:42 +0300

    Added ordering by name in Position class.

--- task_manager/models.py
+++ task_manager/models.py
@@ -13,6 +13,9 @@ TASK_PRIORITIES = [
 class Position(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    class Meta:
+        ordering = ["name"]
+
 
 class TaskType(models.Model):
     name = models.CharField(max_length=255, unique=True)
