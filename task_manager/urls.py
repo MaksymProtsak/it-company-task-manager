e3ebdfd7e079639c0d53256f5883bc986d506862
Maksym Protsak <maksym.protsak@gmail.com>
2024-09-28 13:07:37 +0300

    Changed view for posigion create.

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     index,
     PositionListView,
+    PositionCreateView,
 )
 
 urlpatterns = [
@@ -9,7 +10,7 @@ urlpatterns = [
     path("tasks/", index, name="tasks-list"),
     path("task-types/", index, name="task-types-list"),
     path("positions/", PositionListView.as_view(), name="positions-list"),
-    path("positions/create/", PositionListView.as_view(), name="position-create"),  # Need to change view
+    path("positions/create/", PositionCreateView.as_view(), name="position-create"),  # Need to change view
     path("positions/<int:pk>/update/", PositionListView.as_view(), name="position-update"),  # Need to change view
     path("positions/<int:pk>/delete/", PositionListView.as_view(), name="position-delete"),  # Need to change view
 ]
