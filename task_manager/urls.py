e23867d5b6efcf3b06f5e8f07b3a85dc44ea37ac
Maksym Protsak <maksym.protsak@gmail.com>
2024-09-29 10:56:35 +0300

    Changed views for positon-update and position-delete

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,6 +3,8 @@ from .views import (
     index,
     PositionListView,
     PositionCreateView,
+    PositionUpdateView,
+    PositionDeleteView,
 )
 
 urlpatterns = [
@@ -11,8 +13,8 @@ urlpatterns = [
     path("task-types/", index, name="task-types-list"),
     path("positions/", PositionListView.as_view(), name="positions-list"),
     path("positions/create/", PositionCreateView.as_view(), name="position-create"),
-    path("positions/<int:pk>/update/", PositionListView.as_view(), name="position-update"),  # Need to change view
-    path("positions/<int:pk>/delete/", PositionListView.as_view(), name="position-delete"),  # Need to change view
+    path("positions/<int:pk>/update/", PositionUpdateView.as_view(), name="position-update"),
+    path("positions/<int:pk>/delete/", PositionDeleteView.as_view(), name="position-delete"),
 ]
 
 app_name = "task_manager"
