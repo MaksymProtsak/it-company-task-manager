eee9a6e9d231f6a062f52e87984b2c9e3db3f7ec
Maksym Protsak <maksym.protsak@gmail.com>
2024-09-29 11:49:35 +0300

    Added PositionSearchForm

---
+++ task_manager/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class PositionSearchForm(forms.Form):
+    position = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search position"}
+        )
+    )
