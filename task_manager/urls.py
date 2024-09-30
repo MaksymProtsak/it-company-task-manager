f042f6a4bf56648e8b2e1c57c5ccc135e60e3773
Maksym Protsak <maksym.protsak@gmail.com>
2024-09-30 21:24:36 +0300

    Added pathes for task and task type.

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -5,12 +5,25 @@ from .views import (
     PositionCreateView,
     PositionUpdateView,
     PositionDeleteView,
+    TaskTypeListView,
+    TaskTypeCreateView,
+    TaskTypeUpdateView,
+    TaskTypeDeleteView,
+    TaskListView,
+    TaskCreateView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tasks/", index, name="tasks-list"),
-    path("task-types/", index, name="task-types-list"),
+
+    path("tasks/", TaskListView.as_view(), name="tasks-list"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+
+    path("task-types/", TaskTypeListView.as_view(), name="task-types-list"),
+    path("task-types/create/", TaskTypeCreateView.as_view(), name="task-type-create"),
+    path("task-types/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task-type-update"),
+    path("task-types/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task-type-delete"),
+
     path("positions/", PositionListView.as_view(), name="positions-list"),
     path("positions/create/", PositionCreateView.as_view(), name="position-create"),
     path("positions/<int:pk>/update/", PositionUpdateView.as_view(), name="position-update"),
