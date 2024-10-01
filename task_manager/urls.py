6b630c5d1e9e01a80116a53110c3d1d1b238de86
Maksym Protsak <maksym.protsak@gmail.com>
2024-10-01 13:50:48 +0300

    Added task-update link. Created readable spaces

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -11,23 +11,69 @@ from .views import (
     TaskTypeDeleteView,
     TaskListView,
     TaskCreateView,
+    TaskUpdateView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
 
-    path("tasks/", TaskListView.as_view(), name="tasks-list"),
-    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path(
+        "tasks/",
+        TaskListView.as_view(),
+        name="tasks-list"
+    ),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
 
-    path("task-types/", TaskTypeListView.as_view(), name="task-types-list"),
-    path("task-types/create/", TaskTypeCreateView.as_view(), name="task-type-create"),
-    path("task-types/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task-type-update"),
-    path("task-types/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task-type-delete"),
+    path(
+        "task-types/",
+        TaskTypeListView.as_view(),
+        name="task-types-list"
+    ),
+    path(
+        "task-types/create/",
+        TaskTypeCreateView.as_view(),
+        name="task-type-create"
+    ),
+    path(
+        "task-types/<int:pk>/update/",
+        TaskTypeUpdateView.as_view(),
+        name="task-type-update"
+    ),
+    path(
+        "task-types/<int:pk>/delete/",
+        TaskTypeDeleteView.as_view(),
+        name="task-type-delete"
+    ),
 
-    path("positions/", PositionListView.as_view(), name="positions-list"),
-    path("positions/create/", PositionCreateView.as_view(), name="position-create"),
-    path("positions/<int:pk>/update/", PositionUpdateView.as_view(), name="position-update"),
-    path("positions/<int:pk>/delete/", PositionDeleteView.as_view(), name="position-delete"),
+    path(
+        "positions/",
+        PositionListView.as_view(),
+        name="positions-list"
+    ),
+    path(
+        "positions/create/",
+        PositionCreateView.as_view(),
+        name="position-create"
+    ),
+    path(
+        "positions/<int:pk>/update/",
+        PositionUpdateView.as_view(),
+        name="position-update"
+    ),
+    path(
+        "positions/<int:pk>/delete/",
+        PositionDeleteView.as_view(),
+        name="position-delete"
+    ),
 ]
 
 app_name = "task_manager"
