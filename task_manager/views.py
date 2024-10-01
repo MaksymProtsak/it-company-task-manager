aba275c2cf04e27e3d98c357e7ffd2eded9052d7
Maksym Protsak <maksym.protsak@gmail.com>
2024-10-01 14:04:16 +0300

    Created TaskDeleteView class

--- task_manager/views.py
+++ task_manager/views.py
@@ -130,3 +130,8 @@ class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Task
     form_class = TaskForm
     success_url = reverse_lazy("task_manager:tasks-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("task_manager:tasks-list")
