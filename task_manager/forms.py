81d9696d4d36bb0d941e9b589095b188f9d0806d
Maksym Protsak <maksym.protsak@gmail.com>
2024-10-04 14:21:50 +0300

    Created WorkerCreateForm, WorkerForm, WorkerSearchForm.

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
-from task_manager.models import Task
+from task_manager.models import Task, Worker
 
 
 class PositionSearchForm(forms.Form):
@@ -50,5 +51,45 @@ class TaskSearchForm(forms.Form):
         label="",
         widget=forms.TextInput(
             attrs={"placeholder": "Search a task by name"}
+        )
+    )
+
+
+class WorkerCreateForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "email",
+            "position",
+        )
+
+
+class WorkerForm(forms.ModelForm):
+    class Meta:
+        model = Worker
+        fields = ["username", "first_name", "last_name", "email", "position",]
+        widgets = {
+            "deadline": forms.DateInput(
+                attrs={
+                    "type": "date",
+                }
+            ),
+            "name": forms.TextInput(
+                attrs={
+                    "placeholder": "Write a task name"
+                }
+            )
+        }
+
+
+class WorkerSearchForm(forms.Form):
+    worker = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search worker"}
         )
     )
