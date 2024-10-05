cc002e8d3a516dd9f98c4ac181f4c8069f0d2ff0
Maksym Protsak <maksym.protsak@gmail.com>
2024-10-05 23:55:07 +0300

    Added widget for assignees

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 from task_manager.models import Task, Worker
@@ -27,6 +28,11 @@ class TaskTypeSearchForm(forms.Form):
 
 
 class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Task
         fields = '__all__'
