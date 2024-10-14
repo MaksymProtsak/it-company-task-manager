6bc5fe3718cc10b12b0f67305c33eb64a5f235de
Maksym Protsak <maksym.protsak@gmail.com>
2024-10-14 20:12:34 +0300

    Created SignUpForm.

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from task_manager.models import Task, Worker
+from task_manager.models import Task, Worker, Position
 
 
 class PositionSearchForm(forms.Form):
@@ -99,3 +99,47 @@ class WorkerSearchForm(forms.Form):
             attrs={"placeholder": "Search worker"}
         )
     )
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control"
+            }
+        ))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "Email",
+                "class": "form-control"
+            }
+        ))
+    position = forms.ModelChoiceField(
+        queryset=Position.objects.all(),
+        widget=forms.Select(
+            attrs={
+                "class": "form-control"
+            }
+        ),
+        empty_label="Select Position"
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password check",
+                "class": "form-control"
+            }
+        ))
+
+    class Meta:
+        model = Worker
+        fields = ('username', 'email', 'position', 'password1', 'password2')
