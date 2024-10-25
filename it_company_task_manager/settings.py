a748b468638e7d93a737ebf5810e65758fec0387
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-10-25 08:47:00 +0300

    Update settings adn requirements.txt.

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os, environ
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -21,15 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "default not secure key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != 'False'
 
 # Assets Management
-ASSETS_ROOT = '/static/assets'
+ASSETS_ROOT = "/static/assets"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
